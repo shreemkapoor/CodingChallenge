b6e944d299c7b3a4ee37bb957cdeee09cb495620
Raj <Raj@DESKTOP-5AVB26J>
2019-08-23 14:38:55 +0530

    Push Coding Challenge Project On Git

---
+++ Coding-Challenge/src/com/raj/oops/Test.java
@@ -0,0 +1,34 @@
+package com.raj.oops;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+class Child {
+int i = 10;
+static int staici = 10;
+public void show()
+{
+ System.out.println("Mehodd Show");	
+}
+}
+
+public class Test {
+	public static void main(String[] args) {
+		Child child = new Child();
+		@SuppressWarnings("rawtypes")
+		Class c1 = child.getClass();
+		Method method[] = c1.getDeclaredMethods();
+		for (Method method2 : method) {
+			System.out.println("Methods " + method2);
+		}
+		Field fields[] = c1.getDeclaredFields();
+		for (Field field : fields) {
+			System.out.println(field);
+		}
+		System.out.println(String.class.getClass());
+		System.out.println(String.class.getClasses());
+		System.out.println(Child.class.getClassLoader());
+		
+
+	}
+}
